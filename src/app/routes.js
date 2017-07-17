014710ace9a9f7a1e9f5765178c6c29fc7d73c90
John Char <yannosx@gmail.com>
2017-07-17 22:28:45 +0300

    moved auth route from user routes to routes

--- src/app/routes.js
+++ src/app/routes.js
@@ -4,6 +4,13 @@ const routes = require('express').Router();
 routes.get('/', (req, res) => {
     res.send('api');
 });
+// http://localhost:3030/api/auth
+const authUser = require('./auth').authUser;
+routes
+    .get('/auth', (req, res) => {
+        res.json({ message: 'Send a POST request here with user credentials to authenticate.' });
+    })
+    .post('/auth', authUser);
 
 // http://localhost:3030/api/users and nested routes
 const userRoutes = require('../objects/user/user.routes');
