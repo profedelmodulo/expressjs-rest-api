c43c17692e479e873eab62f5b11c1bf3d281bbd7
John Char <yannosx@gmail.com>
2017-07-19 21:37:51 +0300

    added CRUD controllers for user

--- src/objects/user/user.controller.js
+++ src/objects/user/user.controller.js
@@ -1,25 +1,60 @@
-const userModel = require('./user.model');
+/**
+ * @file Contains the controllers/functions that are used by the user routes.
+ * @author John Char <yannosx@gmail.com> 
+ */
 
-const createUser = (req, res) => {
+// Dependencies
+    const userModel = require('./user.model');
+// ============
 
-    const email = req.body.email;
-    const password = req.body.password;
+// Creates a new user
+    const createUser = (req, res) => {
+        const email = req.body.email;
+        const password = req.body.password;
+        if (!email && !password) res.json({ message: 'No email nor password provided.' });
+        else if (!email) res.json({ message: 'No email provided.' });
+        else if (!password) res.json({ message: 'No password provided.' });
+        else {
+            const newUser = new userModel({
+                email: email,
+                password: password
+            });
+            newUser.save(err => {
+                if (err) return res.json({ message: 'Email already exists.' });
+                res.json({ message: 'User successfully created.' });
+            });
+        }
+    };
+// ==================
 
-    if (!email && !password) res.json({ message: 'No email nor password provided.' });
-    else if (!email) res.json({ message: 'No email provided.' });
-    else if (!password) res.json({ message: 'No password provided.' });
-    else {
-        const newUser = new userModel({
-            email: email,
-            password: password
-        });
-        newUser.save(err => {
-            if (err) return res.json({ message: 'Email already exists.' });
-            res.json({ message: 'User successfully created.' });
-        });
-    }
-};
+// Returns a single user
+    const getSingleUser = (req, res) => {
+        //TODO: add logic
+    };
+// =====================
+
+// Returns all users
+    const getAllUsers = (req, res) => {
+        //TODO: add logic
+    };
+// =====================
+
+// Updates a user
+    const updateUser = (req, res) => {
+        //TODO: add logic
+    };
+// =====================
+
+// Deletes a user
+    const deleteUser = (req, res) => {
+        //TODO: add logic
+    };
+// =====================
 
 module.exports = {
-    createUser: createUser
+    createUser: createUser,
+    getSingleUser: getSingleUser,
+    getAllUsers: getAllUsers,
+    updateUser: updateUser,
+    deleteUser: deleteUser
 };
